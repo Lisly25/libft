34026a630db0470e7574a78653a9e3b0253bc2c2
Szabina Korbai <skorbai@c1r5p8.hive.fi>
2023-11-06 09:54:58 +0200

    Updated ft_calloc.c to correctly allocate size * count memory

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 15:29:14 by skorbai           #+#    #+#             */
-/*   Updated: 2023/11/05 13:22:44 by skorbai          ###   ########.fr       */
+/*   Updated: 2023/11/06 09:48:45 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,18 @@ void	*ft_calloc(size_t count, size_t size)
 
 	i = 0;
 	malloc_size = size * count;
-	if ((count != 0 && malloc_size / count != size))
+	if (count != 0 && (malloc_size / count != size))
 		return (NULL);
 	ptr = malloc(malloc_size);
+	printf("We have malloced %zu size\n", malloc_size);
 	if (ptr == NULL)
 		return (NULL);
 	buffer = (char *) ptr;
-	while (count != 0)
+	while (malloc_size != 0)
 	{
 		buffer[i] = 0;
 		i++;
-		count--;
+		malloc_size--;
 	}
 	return (ptr);
 }
