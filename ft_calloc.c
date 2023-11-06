66d585164652ad037c2887706f8efab83326b91b
Szabina Korbai <skorbai@c1r5p8.hive.fi>
2023-11-06 10:10:51 +0200

    Removed unnecessary headers and leftover debugging code from ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,13 +6,11 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 15:29:14 by skorbai           #+#    #+#             */
-/*   Updated: 2023/11/06 09:48:45 by skorbai          ###   ########.fr       */
+/*   Updated: 2023/11/06 10:06:09 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <limits.h>
-#include <stdio.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
@@ -26,7 +24,6 @@ void	*ft_calloc(size_t count, size_t size)
 	if (count != 0 && (malloc_size / count != size))
 		return (NULL);
 	ptr = malloc(malloc_size);
-	printf("We have malloced %zu size\n", malloc_size);
 	if (ptr == NULL)
 		return (NULL);
 	buffer = (char *) ptr;
