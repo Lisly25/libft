ff8081a730ccd0b7b574cd2fd71a0942b9f88f11
Szabina Korbai <skorbai@c1r5p8.hive.fi>
2023-11-20 16:00:27 +0200

    ft_lstclear_bonus.c now correctly sets the list to NULL

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 16:05:19 by skorbai           #+#    #+#             */
-/*   Updated: 2023/11/20 11:12:06 by skorbai          ###   ########.fr       */
+/*   Updated: 2023/11/20 15:57:37 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,12 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 	t_list	*copy_of_lst;
 
 	copy_of_lst = *lst;
-	next_node = copy_of_lst->next;
-	if (next_node != NULL && copy_of_lst->next == NULL)
-	{
-		ft_lstdelone(copy_of_lst, del);
-		return ;
-	}
-	while (next_node != NULL)
+	while (copy_of_lst != NULL)
 	{
 		next_node = copy_of_lst->next;
-		if (next_node != NULL)
-			ft_lstclear(&next_node, del);
+		del(copy_of_lst->content);
+		free(copy_of_lst);
+		copy_of_lst = next_node;
 	}
-	lst = NULL;
+	*lst = NULL;
 }
