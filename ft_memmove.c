d261ea273bbdf0218f55dd3d1af33256a042dd67
Szabina Korbai <skorbai@c1r5p8.hive.fi>
2023-11-21 10:40:48 +0200

    Simplified and fixed some issues with the read size in ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,78 +6,35 @@
 /*   By: skorbai <skorbai@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 15:08:20 by skorbai           #+#    #+#             */
-/*   Updated: 2023/11/20 17:13:21 by skorbai          ###   ########.fr       */
+/*   Updated: 2023/11/21 10:36:27 by skorbai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	unsign_len(unsigned char *s)
-{
-	size_t			i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-static int	do_strs_overlap(unsigned char *dst, unsigned char *src)
-{
-	if (dst + unsign_len(dst) == src + unsign_len(src))
-		return (1);
-	return (0);
-}
-
-static void	*overlap_memmove(unsigned char *dst, unsigned char *src, size_t len)
-{
-	if (unsign_len(src) < unsign_len(dst))
-	{
-		while (len != 0)
-		{
-			*dst = *src;
-			len--;
-			dst++;
-			src++;
-		}
-	}
-	else
-	{
-		dst = dst + (len - 1);
-		src = src + (len - 1);
-		while (len != 0)
-		{
-			*dst = *src;
-			len--;
-			dst--;
-			src--;
-		}
-	}
-	return (dst);
-}
-
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char	*buffer_d;
-	unsigned char	*buffer_s;
+	size_t				i;
+	size_t				j;
+	unsigned char		*buffer_d;
+	const unsigned char	*buffer_s;
 
-	buffer_s = (unsigned char *) src;
-	buffer_d = (unsigned char *) dst;
-	if ((src == NULL && dst == NULL) || len == 0)
+	i = 0;
+	j = len - 1;
+	buffer_s = src;
+	buffer_d = dst;
+	if ((dst == NULL && src == NULL) || len == 0)
 		return (dst);
-	if (do_strs_overlap(buffer_d, buffer_s) == 1)
-		overlap_memmove(buffer_d, buffer_s, len);
-	else
+	while (buffer_d < buffer_s && i < len)
 	{
-		buffer_d = buffer_d + (len - 1);
-		buffer_s = buffer_s + (len - 1);
-		while (len != 0)
-		{
-			*buffer_d = *buffer_s;
-			len--;
-			buffer_d--;
-			buffer_s--;
-		}
+		buffer_d[i] = buffer_s[i];
+		i++;
+	}
+	while (buffer_s < buffer_d && len != 0)
+	{
+		buffer_d[j] = buffer_s[j];
+		j--;
+		len--;
 	}
 	return (dst);
 }
